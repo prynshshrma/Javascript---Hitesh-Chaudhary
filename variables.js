5d69b24c4399e28c34c335c094d60155bcb282a8
prynshshrma <sharmapriyansh056@gmail.com>
2025-07-28 13:14:29 +0530

    third change by creating the variable file

---
+++ variables.js
@@ -0,0 +1,12 @@
+const accountId = 7484
+let emailId = "priyansh@gmail.com"
+
+let state; // it is treated as undefined as javascript
+
+// accountId = 90 
+
+// var must not be used because it is functional scoped rather than block scoped   this means if we change a variable declared with var it also affects all other same variables throughout the entire code
+
+console.log(accountId);
+console.table([accountId, emailId, state])
+
