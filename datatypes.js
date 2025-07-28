3a93d8b5ea8a746971d8501413f7dcf9b0d74c71
prynshshrma <sharmapriyansh056@gmail.com>
2025-07-28 15:21:52 +0530

    dataypes

---
+++ datatypes.js
@@ -0,0 +1,31 @@
+"use strict" // treat al the JS code as newer version of Javascript
+
+let age = 18 // number
+let name = "priyansh" // string
+let isLoggedin = true // boolean
+ // alert("hello")
+
+let myName; // it is undefined 
+
+// null and undefined are two different aspects that looks same
+
+let temp = null; // null is the representation of empty space or value in the memory
+console.log(temp);
+
+// symbol => it used to state the uniqueness among elements
+
+
+
+
+
+
+
+
+
+// OBJECTS
+
+console.log(typeof myName);  // typeof used to find the type of the element
+console.log(typeof age); 
+
+
+
