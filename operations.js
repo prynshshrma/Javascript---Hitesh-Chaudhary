7d0e6c29f5ee05923f9477e6794a8a311c16373e
prynshshrma <sharmapriyansh056@gmail.com>
2025-07-29 22:36:06 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,46 @@
+// console.log(2+2);
+// console.log(2-2);
+console.log(2**2);  // 2 raise 2
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 3);
+console.log("3" + 3);
+console.log("3" + 3 + 4); // this is not the desired way to write the code
+console.log((3 + 4) - 2 % 4);  // use of curly brackets () is must
+
+console.log(+true);
+console.log(+"");
+
+
+
+
+// prefix
+// first increment and then return
+
+
+x = 3;
+y = ++x;
+console.log(x);
+console.log(y);
+
+
+// postfix
+// first returns then increment
+
+x = 3;
+y = x++;
+console.log(x);
+console.log(y);
+
+
+
+
+
+
+
