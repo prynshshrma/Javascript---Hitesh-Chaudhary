41dd153000aa11bdbe5da094e7a47adae4114ea4
prynshshrma <sharmapriyansh056@gmail.com>
2025-07-31 23:28:44 +0530

    string methods

---
+++ string.js
@@ -0,0 +1,29 @@
+let a = "hello"
+let b = "world"
+// console.log(a + " " + b + 90);
+
+// String interpolation is the modern way to add variables in string enclosed with backticks `` 
+
+console.log(`Hello my name is ${a} and I am ${b}`);
+
+console.log(a[1]); // accessing key values 
+
+// Methods or Functions
+
+console.log(a.length); // indetifying length
+
+console.log(a.toUpperCase()); // converting uppercase
+
+console.log(a.substring(0, 3)); // diving into substrings
+
+
+let newStr = "      priyansh   .   "
+console.log(newStr);
+console.log(newStr.trim());  // trim() used to remove the unusual spaces in the user queries 
+ 
+
+
+
+
+
+
