6e12546f38cb0f7bc355f827b7af3ddff5fd0f89
prynshshrma <sharmapriyansh056@gmail.com>
2025-09-07 19:32:12 +0530

    loops

---
+++ loops/one.js
@@ -0,0 +1,33 @@
+// for loop
+
+
+for (let i = 1; i <= 10; i++) {
+    // console.log(i);
+
+}
+
+
+// break
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        console.log("5 is here!");
+        break;
+    }
+
+    console.log(index);
+    
+    
+}
+
+// continue
+
+for (let index = 1; index <= 20; index++) {
+    if (index == 5) {
+        continue
+    }
+
+    console.log(index);
+    
+    
+}
---
+++ loops/two.js
@@ -0,0 +1,24 @@
+// while
+
+let index = 1
+while (index <= 10) {
+    // console.log(index);
+    index++
+}
+
+let arr = ["batman", "superman", "hulk"]
+let i = 0
+while(i<arr.length){
+    console.log(arr[i]);
+    i++
+}
+
+
+
+// do while
+
+let score = 90
+do {
+    console.log(score);
+    
+} while (score<=10);
